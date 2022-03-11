ec0dec9de6f8339d2964728dcd9bc03671c79133
vishnu-3001 <vishnusai3001@yahoo.com>
2022-03-11 10:50:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit} from '@angular/core';
+import { UserService } from './user.service';
+import { Subject } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'poc1';
+  constructor(private user:UserService){}
+  nub=10;
+  userdet:any=[];
+  userfulldet:any={};
+  detform:FormGroup;
+  reqid:number;
+  ngOnInit(){
+    this.detform=new FormGroup({
+      id:new FormControl()
+    })
+      this.user.getData().subscribe((data:any)=>{
+        this.userdet=data;
+      })
+  }
+  submitted(){
+     this.reqid=this.detform.value['id'];
+     console.log(this.reqid);
+    this.userfulldet=this.userdet[this.reqid];
+  }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,13 @@
+import { Subject } from "rxjs";
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+    providedIn:'root',
+})
+export class UserService{
+    constructor (private http:HttpClient){
+    }
+    getData(){
+        return this.http.get("https://jsonplaceholder.typicode.com/todos");
+    }
+}
